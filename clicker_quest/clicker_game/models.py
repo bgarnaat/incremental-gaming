c70b3ffee619548952e7b718bc926730ff63cb2f
Kyle R <KyleRichVA@gmail.com>
2016-05-02 13:47:26 -0700

    #3 Created django model for game instance'

--- clicker_quest/clicker_game/models.py
+++ clicker_quest/clicker_game/models.py
@@ -2,15 +2,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.conf import settings
+import datetime
+import json
 
 # Create your models here.
 
 
 class Clicker_Game(models.Model):
-    owner = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                 on_delete=models.CASCADE,
-                                 related_name='game')
+    """Model for a general clicker game."""
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name='games_owned')
     game_data = models.TextField()
     modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200)
+
+
+class Game_Instance(models.Model):
+    """Model for a single clicker game instance/state"""
+    class meta:
+        unique_together = ('user', 'game')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='games_playing')
+    game = models.ForeignKey(Clicker_Game, related_name='running_games')
+    data = models.TextField()
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
