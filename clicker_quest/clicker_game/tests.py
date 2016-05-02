498f61dbdec0527a84d0a5ec5b130ffa02dc9a33
Kyle R <KyleRichVA@gmail.com>
2016-05-02 11:15:13 -0700

    #3 Switched over to Database URL environ with dj_database_url and started tests.

--- clicker_quest/clicker_game/tests.py
+++ clicker_quest/clicker_game/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.conf import settings
+import factory
 
 # Create your tests here.
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """Test using factory for user model.."""
+
+    class Meta:
+        """Establish User model as the product of this factory."""
+
+        model = settings.AUTH_USER_MODEL
+        django_get_or_create = ('username',)
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    username = factory.LazyAttribute(
+        lambda object: ''.join((object.first_name, object.last_name)))
+    password = factory.PostGenerationMethodCall('set_password', 'password')
+
+
+class GameTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory.create()
+
+    def test_game_exists(self):
+        """Test a game can be made with the proper properties."""
+        import pdb; pdb.set_trace()
