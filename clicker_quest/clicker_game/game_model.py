c0687871a354cd51462851865d9ed6adcdc56195
Kent Ross <root.main@gmail.com>
2016-05-03 15:26:31 -0700

    Second pass on game_model, now with validation

--- clicker_quest/clicker_game/game_model.py
+++ clicker_quest/clicker_game/game_model.py
@@ -9,6 +9,121 @@ class Dicted(object):
             setattr(self, key, value)
 
 
+def validate_game_model(data):
+
+    try:
+        model = GameModel(data)
+
+        def validate_resource_amounts(cost_data):
+            for resource_name, amount in cost_data.values():
+                if resource_name not in model.resources:
+                    raise ValueError("Nonexistent resource specified", resource_name)
+                if not isinstance(amount, (int, float)):
+                    raise ValueError("Non-numeric resource amount", resource_name, amount)
+
+        def validate_unlock(unlock):
+            if unlock is ():
+                return
+            if not isinstance(unlock, dict):
+                raise ValueError("Unlock must be an object with keys and values")
+            if not all(x in ('buildings', 'upgrades') for x in unlock):
+                raise ValueError("Extra values in unlock", unlock)
+            if unlock:
+                for building_name, number in unlock['buildings']:
+                    if building_name not in model.buildings:
+                        raise ValueError("Unlock references nonexistent building", building_name)
+                    if not isinstance(number, (int, float)):
+                        raise ValueError("Non-numeric required number of buildings in unlock", number)
+                for upgrade_name in unlock['upgrades']:
+                    if upgrade_name not in model.upgrades:
+                        raise ValueError("Unlock references nonexistent upgrade", upgrade_name)
+
+        if len(model.ordered_resources) != len(model.resources):
+            raise ValueError("Two resources share the same name")
+        if len(model.ordered_buildings) != len(model.buildings):
+            raise ValueError("Two buildings share the same name")
+        if len(model.ordered_upgrades) != len(model.upgrades):
+            raise ValueError("Two upgrades share the same name")
+
+        for resource in model.resources.values():
+            if not isinstance(resource.maximum, (int, float, type(None))):
+                raise ValueError("A resource has a non-numerical maximum")
+
+        for building in model.buildings.values():
+            validate_unlock(building.unlock)
+            validate_resource_amounts(building.cost)
+            if not isinstance(building.cost_factor, (int, float)):
+                raise ValueError("Non-numeric cost factor for building", building.name, building.cost_factor)
+            validate_resource_amounts(building.income)
+            validate_resource_amounts(building.storage)
+            for resource_name in building.storage:
+                if building.resources[resource_name].maximum is None:
+                    raise ValueError("Building has storage for an unlimited resource", building.name, resource_name)
+
+        for upgrade in model.upgrades.values():
+            validate_unlock(upgrade.unlock)
+            validate_resource_amounts(upgrade.cost)
+            for building_name, effects in upgrade().buildings:
+                if building_name not in model.buildings:
+                    raise ValueError("Upgrade affects cost of nonexistent building", upgrade.name, building_name)
+                for effect_type in effects:
+                    if effect_type not in ('cost', 'income'):
+                        raise ValueError(
+                            "Unknown effect specified for upgrade",
+                            upgrade.name, building_name, effect_type
+                        )
+                if 'cost' in effects:
+                    for resource_name, multiplier in effects['cost']:
+                        if resource_name not in model.resources:
+                            raise ValueError(
+                                "Upgrade affects cost for nonexistent resource",
+                                upgrade.name, building_name, resource_name
+                            )
+                        if not isinstance(multiplier, (int, float)):
+                            raise ValueError(
+                                "Non-numeric building cost multiplier in an upgrade",
+                                upgrade.name, building_name, resource_name, multiplier
+                            )
+                if 'income' in effects:
+                    for resource_name, income in effects['cost']:
+                        if resource_name not in model.resources:
+                            raise ValueError(
+                                "Upgrade affects cost for nonexistent resource",
+                                upgrade.name, building_name, resource_name
+                            )
+                        if not isinstance(income, (int, float)):
+                            raise ValueError(
+                                "Non-numeric building income multiplier in an upgrade",
+                                upgrade.name, building_name, resource_name, income
+                            )
+
+            for key in model.new_game:
+                if key not in ('resources', 'buildings', 'upgrades'):
+                    raise ValueError("Invalid key in new game state", key)
+                if 'resources' in model.new_game:
+                    validate_resource_amounts(model.new_game['resources'])
+                if 'buildings' in model.new_game:
+                    for building_name, number in model.new_game['buildings'].items():
+                        if building_name not in model.buildings:
+                            raise ValueError("Nonexistent building in new game state", building_name)
+                        if not isinstance(number, (int, float)):
+                            raise ValueError(
+                                "Non-numeric number of buildings in new game state",
+                                building_name, number
+                            )
+                if 'upgrades' in model.new_game:
+                    if not isinstance(model.new_game['upgrades'], list):
+                        raise ValueError("Upgrades value in new game state is not a list")
+                    for upgrade_name in model.new_game['upgrades']:
+                        if upgrade_name not in model.upgrades:
+                            raise ValueError("Nonexistent upgrade in new game state", upgrade_name)
+
+    except KeyError as ex:
+        raise ValueError("Missing key", ex.args)
+    except AttributeError as ex:
+        raise ValueError("Wrong type of value somewhere", ex.args)
+
+
 class GameModel(object):
     def __init__(self, data):
         """
@@ -16,18 +131,28 @@ class GameModel(object):
 
         May raise a ValueError if the model does not validate.
         """
+        # game info
+        self.name = data.pop('name')
         self.description = data.pop('description')
+
         # resources
-        self.resources = {
-            name: Dicted(
+        self.ordered_resources = []
+        self.resources = {}
+        for resource in data.pop('resources'):
+            resource = Dicted(
+                name=self.resources['name'],
                 description=resource.get('description', ""),
                 maximum=resource.get('maximum'),
             )
-            for name, resource in data.pop('resources').items()
-        }
+            self.resources[resource.name] = resource
+            self.ordered_resources.append(resource)
+
         # buildings
-        self.buildings = {
-            name: Dicted(
+        self.ordered_buildings = []
+        self.buildings = {}
+        for building in data.pop('buildings'):
+            building = Dicted(
+                name=building['name'],
                 description=building.get('description', ""),
                 unlock=building.get('unlock', ()),
                 cost=building['cost'],
@@ -35,18 +160,23 @@ class GameModel(object):
                 income=building.get('income', {}),
                 storage=building.get('storage', {}),
             )
-            for name, building in data.pop('buildings').items()
-        }
+            self.buildings[building.name] = building
+            self.ordered_buildings.append(building)
+
         # upgrades
-        self.upgrades = {
-            name: Dicted(
+        self.ordered_upgrades = []
+        self.upgrades = {}
+        for upgrade in data.pop('upgrades'):
+            upgrade = Dicted(
+                name=upgrade['name'],
                 description=upgrade.get('description', ""),
                 unlock=upgrade.get('unlock', ()),
                 cost=upgrade['cost'],
                 buildings=upgrade.get('buildings', {}),
             )
-            for name, upgrade in data.pop('upgrades').items()
-        }
+            self.upgrades[upgrade.name] = upgrade
+            self.ordered_upgrades.append(upgrade)
+
         # new game game-state
         self.new_game = data.pop('new_game')
 
@@ -88,7 +218,8 @@ class GameInstance(object):
         buildings if possible, and return the (modified game state, and data to pass to the client) in a tuple
         """
         self.fast_forward(current_time)
-        # todo: attempt purchase of a building
+        if self.pay_cost(self.cost_of_building(building_name, number_to_buy=number_purchased)):
+            self.acquire_building(building_name, number_purchased)
         return self.save_state_json(), self.client_state_json()
 
     def purchase_upgrade(self, current_time, upgrade_name):
@@ -97,7 +228,8 @@ class GameInstance(object):
         upgrade if possible, and return the (modified game state, and data to pass to the client) in a tuple
         """
         self.fast_forward(current_time)
-        # todo: attempt purchase of an upgrade
+        if self.pay_cost(self.model.upgrades[upgrade_name].cost):
+            self.upgrades.add(upgrade_name)
         return self.save_state_json(), self.client_state_json()
 
     def save_state_json(self):
@@ -108,7 +240,7 @@ class GameInstance(object):
         if self.buildings:
             result['buildings'] = {name: building.owned for name, building in self.buildings.items()}
         if self.upgrades:
-            result['upgrades'] = self.upgrades.copy()
+            result['upgrades'] = list(self.upgrades)
         return result
 
     def client_state_json(self):
@@ -116,14 +248,46 @@ class GameInstance(object):
         Return the information about the game state suitable for the client side JS to render
         the page we want the user to see
         """
-        return {"todo": "show some stuff to the client"}  # TODO
+        result = {}
+        # resources
+        if self.resources:
+            result['resources'] = {
+                resource_name: {
+                    'description': self.model.resources[resource_name].description,
+                    'owned': resource.owned,
+                    'income': resource.income,
+                    'maximum': resource.maximum,
+                }
+                for resource_name, resource in self.resources.items()
+            }
+
+        # buildings
+        result['buildings'] = {}
+        for building_name, building in self.model.buildings.items():
+            owned = building_name in self.buildings and self.buildings[building_name].owned or 0
+            income = owned and self.buildings[building_name].income or 0.0
+            if owned or self.requirement_is_met(building.unlock):
+                result['buildings'][building_name] = {
+                    'description': building.description,
+                    'owned': owned,
+                    'cost': self.cost_of_building(building_name, 1),
+                    'cost10': self.cost_of_building(building_name, 10),
+                    'income': income,
+                }
+
+        # upgrades
+        result['upgrades'] = {}
+        for upgrade_name, upgrade in self.model.upgrades.items():
+            if upgrade_name in self.upgrades or self.requirement_is_met(upgrade.unlock):
+                result['upgrades'][upgrade_name] = {
+                    'description': upgrade.description,
+                    'owned': upgrade_name in self.upgrades,
+                    'cost': upgrade.cost,
+                }
+
+        return result
 
     def calculate_values(self):
-        for name, resource in self.resources.items():
-            resource.income = 0.0
-            if self.model.resources[name].maximum is not None:
-                resource.maximum = self.model.resources[name].maximum
-
         # calculate resource incomes per building type
         for name, building in self.buildings.items():
             # incomes are per resource
@@ -143,12 +307,54 @@ class GameInstance(object):
                 if 'income' in effects:
                     for resource, income_multiplier in effects['income']:
                         building.income[resource] *= income_multiplier
+
+        # reset resources maximums and incomes
+        for name, resource in self.resources.items():
+            resource.income = 0.0
+            resource.maximum = self.model.resources[name].maximum
+
         # calculate total storage and income right now
         for building in self.buildings.values():
             for resource, storage in building.storage.items():
-                self.resources[resource].maximum += storage * building.owned
+                self.acquire_storage(resource, storage * building.owned)
             for resource, income in building.income.items():
                 self.resources[resource].income += income * building.owned
+
+    def acquire_resource(self, resource_name, amount):
+        """Add an amount of a resource to the state"""
+        if resource_name not in self.resources:
+            self.resources[resource_name] = Dicted(owned=0.0, maximum=self.model.resources[resource_name].maximum)
+        self.resources[resource_name].owned += amount
+
+    def acquire_storage(self, resource_name, storage):
+        """Add an amount of storage for a resource to the state"""
+        if resource_name not in self.resources:
+            self.resources[resource_name] = Dicted(
+                owned=0.0,
+                income=0.0,
+                maximum=self.model.resources[resource_name].maximum
+            )
+        self.resources[resource_name].maximum += storage
+
+    def acquire_income(self, resource_name, income):
+        """Add an amount of income to the calculated state"""
+        if resource_name not in self.resources:
+            self.resources[resource_name] = Dicted(
+                owned=0.0,
+                income=0.0,
+                maximum=self.model.resources[resource_name].maximum
+            )
+        self.resources[resource_name].income += income
+
+    def acquire_building(self, building_name, number):
+        """Add a number of buildings to the state"""
+        if building_name not in self.buildings:
+            self.buildings[building_name] = Dicted(owned=0)
+        self.buildings[building_name].owned += number
+
+    def acquire_upgrade(self, upgrade_name):
+        """Add an upgrade to this game state"""
+        self.upgrades.add(upgrade_name)
 
     def fast_forward(self, current_time):
         """Fast forward the time of the game state to the given time"""
@@ -164,9 +370,41 @@ class GameInstance(object):
         to unlock a specific thing and return True if those requirements are met (False otherwise).
         """
         if 'buildings' in unlock:
-            if not all(self.buildings[building].owned >= count for building, count in unlock['buildings']):
+            if not all(
+                building in self.buildings and self.buildings[building].owned >= count
+                for building, count in unlock['buildings']
+            ):
                 return False
         if 'upgrades' in unlock:
-            if not all(upgrade in self.upgrades for upgrade in unlock['upgrades']):
+            if not all(
+                upgrade in self.upgrades
+                for upgrade in unlock['upgrades']
+            ):
                 return False
+        return True
+
+    def cost_of_building(self, building_name, number_to_buy=1):
+        """Calculate the cost of purchasing a certain number of a building"""
+        if building_name not in self.buildings:
+            raise KeyError(building_name)
+        building = self.buildings[building_name]
+        result = {resource: 0.0 for resource in building.cost}
+        for resource, amount in building.cost.items():
+            for n in range(building.owned, building.owned + number_to_buy):
+                result[resource] += amount * self.model.buildings[building_name].cost_factor ** n
+        return result
+
+    def cost_is_affordable(self, cost):
+        """Determine whether a cost is currently affordable"""
+        return all(
+            resource in self.resources and self.resources[resource].owned >= amount
+            for resource, amount in cost.items()
+        )
+
+    def pay_cost(self, cost):
+        """If a cost is affordable, pay the cost and return True. Otherwise, return False."""
+        if not self.cost_is_affordable(cost):
+            return False
+        for resource, amount in cost.items():
+            self.resources[resource].owned -= amount
         return True
