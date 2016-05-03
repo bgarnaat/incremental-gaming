d51f60022710608025db5aa236e63768883790b6
Kent Ross <root.main@gmail.com>
2016-05-03 15:30:42 -0700

    tests file added

---
+++ clicker_quest/clicker_game/test_game_model.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from django.test import TestCase
+
+from clicker_game.game_model import GameModel, GameInstance
+
+
+class GameModelTest(TestCase):
+    def setUp(self):
+        pass
+
+    # todo validation tests
+    # todo calculation tests
