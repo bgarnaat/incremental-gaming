b3614106f52c034e03ee3bf500ea7d24d5c3ebf9
Kent Ross <root.main@gmail.com>
2016-05-04 14:55:36 -0700

    some more passing validation tests

--- clicker_quest/clicker_game/test_game_model.py
+++ clicker_quest/clicker_game/test_game_model.py
@@ -15,9 +15,13 @@ class GameModelValidationTest(TestCase):
             'new_game': {},
         }
 
-    def dont_validate(self):
-        with self.assertRaises(ValueError):
+    def dont_validate(self, message):
+        try:
             validate_game_model(self.game)
+        except ValueError as ex:
+            self.assertIn(message, ex.args[0])
+        else:
+            self.assertFalse("validation did not fail")
 
     def validate_ok(self):
         validate_game_model(self.game)
@@ -28,44 +32,44 @@ class GameModelValidationTest(TestCase):
     
     def test_missing_keys_model(self):
         self.game.pop('upgrades')
-        self.dont_validate()
+        self.dont_validate("Missing or extra keys")
     
     def test_extra_keys_model(self):
         self.game['invalid_extra_key'] = 5
-        self.dont_validate()
+        self.dont_validate("Missing or extra keys")
 
     def test_unnamed_resource(self):
-        self.game['resources'].append({1: "unnamed"})
-        self.dont_validate()
+        self.game['resources'].append({'maximum': 100})
+        self.dont_validate("Missing key")
 
     def test_unnamed_building(self):
-        self.game['buildings'].append({1: "unnamed"})
-        self.dont_validate()
+        self.game['buildings'].append({'cost': {}, 'cost_factor': 2})
+        self.dont_validate("Missing key")
 
     def test_unnamed_upgrade(self):
-        self.game['upgrades'].append({1: "unnamed"})
-        self.dont_validate()
+        self.game['upgrades'].append({'cost': {}, 'buildings': {}})
+        self.dont_validate("Missing key")
     
     def test_overload_resources(self):
         entry = {'name': "abc"}
         self.game['resources'].append(entry)
         self.validate_ok()
         self.game['resources'].append(entry)
-        self.dont_validate()
+        self.dont_validate("Two resources share the same name")
     
     def test_overload_buildings(self):
-        entry = {'name': "abc", 'cost': {}}
+        entry = {'name': "abc", 'cost': {}, 'cost_factor': 2}
         self.game['buildings'].append(entry)
         self.validate_ok()
         self.game['buildings'].append(entry)
-        self.dont_validate()
+        self.dont_validate("Two buildings share the same name")
     
     def test_overload_upgrades(self):
         entry = {'name': "abc", 'cost': {}}
         self.game['upgrades'].append(entry)
         self.validate_ok()
         self.game['upgrades'].append(entry)
-        self.dont_validate()
+        self.dont_validate("Two upgrades share the same name")
 
     def test_valid_resources(self):
         self.game['resources'].extend([
@@ -78,28 +82,104 @@ class GameModelValidationTest(TestCase):
         self.game['resources'].append(
             {'name': "abc", 'maximum': "not a number"}
         )
-        self.dont_validate()
+        self.dont_validate("non-numeric maximum")
 
     def test_non_dict_unlock(self):
         self.game['buildings'].append(
-            {'name': "abc", 'unlock': [6], 'cost': {}}
+            {
+                'name': "abc",
+                'unlock': [6],
+                'cost': {},
+                'cost_factor': 2,
+            }
         )
-        self.dont_validate()
+        self.dont_validate("Unlock must be a json object")
 
     def test_unlock_extra_keys(self):
-        pass  # todo
+        self.game['buildings'].append(
+            {
+                'name': "abc",
+                'unlock': {'invalid key': 1},
+                'cost': {},
+                'cost_factor': 2,
+            }
+        )
+        self.dont_validate("Extra values in unlock")
 
     def test_unlock_invalid_building(self):
-        pass  # todo
+        self.game['buildings'].append(
+            {
+                'name': "abc",
+                'unlock': {
+                    'buildings': {"def": 1}
+                },
+                'cost': {},
+                'cost_factor': 2,
+            }
+        )
+        self.dont_validate("references nonexistent building")
 
     def test_unlock_non_numeric_building_number(self):
-        pass  # todo
+        self.game['buildings'].append(
+            {
+                'name': "abc",
+                'unlock': {
+                    'buildings': {"abc": "not a number"}
+                },
+                'cost': {},
+                'cost_factor': 2,
+            }
+        )
+        self.dont_validate("Non-numeric number of required buildings")
 
     def test_unlock_invalid_upgrade(self):
-        pass  # todo
+        self.game['buildings'].append(
+            {
+                'name': "abc",
+                'unlock': {
+                    'upgrades': ["nonexistent"]
+                },
+                'cost': {},
+                'cost_factor': 2,
+            }
+        )
+        self.dont_validate("references nonexistent upgrade")
 
     def test_good_unlock(self):
-        pass  # todo
+        self.game['buildings'].extend([
+            {
+                'name': "abc",
+                'unlock': {
+                    'buildings': {"abc": 1},
+                },
+                'cost': {},
+                'cost_factor': 2,
+            },
+            {
+                'name': "def",
+                'unlock': {
+                    'upgrades': ["upgrade"],
+                },
+                'cost': {},
+                'cost_factor': 2,
+            },
+            {
+                'name': "ghi",
+                'unlock': {
+                    'buildings': {"abc": 1},
+                    'upgrades': ["upgrade"],
+                },
+                'cost': {},
+                'cost_factor': 2,
+            },
+        ])
+        self.game['upgrades'].append(
+            {
+                'name': "upgrade",
+                'cost': {},
+            }
+        )
+        self.validate_ok()
 
     def test_building_without_cost(self):
         pass  # todo
