cac6eae1967b7cfc71c5750b6bf414a2aefcf9d4
Kyle R <KyleRichVA@gmail.com>
2016-05-03 17:01:56 -0700

    changed over views to be able to only deal with post requests that are ajax. Local tests can update and return database stuff without refreshing page.

--- clicker_quest/clicker_game/views.py
+++ clicker_quest/clicker_game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from clicker_game.models import Game_Instance, Clicker_Game
 from clicker_game.game_model import GameModel
@@ -10,18 +10,19 @@ from clicker_game.game_model import GameModel
 
 
 class MainView(View):
-    """The View Used when loading a game.
+    """The View Used for a clicker game.
 
     if a user is logged in it returns their game instance.
     Otherwise give a new game instance.
+    All post requests are done by ajax requests.
     """
     template_name = 'base.html'
 
     def get(self, request):
         game_model_data = Clicker_Game.objects.all()[0].game_data  # Get The Game Rules the User Is Playing
-        model = GameModel(game_model_data)
-        instance = model.load_game_instance()
-
+        #model = GameModel(game_model_data)
+        #instance = model.load_game_instance()
+        instance = game_model_data
         if request.user.is_authenticated():
             cur_vals = Game_Instance.objects.get(user=request.user).data
             return render(request, self.template_name, {'game': instance, 'vals': cur_vals})
@@ -29,22 +30,15 @@ class MainView(View):
             start_vals = game_model_data['new_game']
             return render(request, self.template_name, {'game': instance, 'vals': start_vals})
 
-
-def purchase_building(request, building_name, num_purchased):
-    game_model_data = Clicker_Game.objects.all()[0].game_data  # Get The Game Rules the User Is Playing
-    instance = GameModel(game_model_data).load_game_instance()
-    new_data = instance.purchase_building(request.GET['Date'], building_name, num_purchased)
-    game_instance = Game_Instance.objects.get(user=request.user)
-    game_instance.data = new_data
-    game_instance.save()
-    return render(request, 'base.html', {'game': instance, 'vals': game_instance.data})
-
-
-def purchase_upgrade(request, upgrade_name):
-    game_model_data = Clicker_Game.objects.all()[0].game_data  # Get The Game Rules the User Is Playing
-    instance = GameModel(game_model_data).load_game_instance()
-    new_data = instance.purchase_upgrade(request.GET['Date'], upgrade_name)
-    game_instance = Game_Instance.objects.get(user=request.user)
-    game_instance.data = new_data
-    game_instance.save()
-    return render(request, 'base.html', {'game': instance, 'vals': game_instance.data})
+    def post(self, request):
+        #import pdb; pdb.set_trace()
+        if 'building' in request.POST:
+            pass  # purchase_building
+        elif 'upgrade' in request.POST:
+            pass  # purchase_upgrade
+        elif 'clicker' in request.POST:
+            game_rules = Clicker_Game.objects.all()[0].game_data
+            game_instance = Game_Instance.objects.get(user=request.user)
+            game_instance.data['clicks'] += game_rules['clicked']
+            game_instance.save()
+            return JsonResponse({'vals': game_instance.data})
