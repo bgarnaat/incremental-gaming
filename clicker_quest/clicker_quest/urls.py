5a41e1c43eabe4523a2b225e4aaba45e23c0ebff
Kyle R <KyleRichVA@gmail.com>
2016-05-05 13:20:49 -0700

    added base css and setup links to proper pages.

--- clicker_quest/clicker_quest/urls.py
+++ clicker_quest/clicker_quest/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^logout', logged_out),
     url(r'^accounts/profile', logged_in),
-    url(r'^accounts/register', UserRegistration.as_view()),
+    url(r'^accounts/register', UserRegistration.as_view(), name='register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
