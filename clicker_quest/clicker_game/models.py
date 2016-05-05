0b11e1a1050caf0cf7cb178fe9114108fb6cd904
Kent Ross <root.main@gmail.com>
2016-05-05 16:50:58 -0700

    validate game model before saving

--- clicker_quest/clicker_game/models.py
+++ clicker_quest/clicker_game/models.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.contrib.postgres.fields import JSONField
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
+from clicker_game.game_model import validate_game_model
 
 # Create your models here.
 
@@ -16,6 +20,11 @@ class ClickerGame(models.Model):
     modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200)
+
+
+@receiver(pre_save, sender=ClickerGame)
+def validate_game(sender, **kwargs):
+    validate_game_model(kwargs['instance'].game_data)
 
 
 class GameInstance(models.Model):
